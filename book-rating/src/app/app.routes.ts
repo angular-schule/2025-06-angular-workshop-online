c731acda73c8ae4eeba1c2978718cf8208934f96
Ferdinand Malcher <ferdinand@malcher.media>
2025-06-30 15:02:21 +0200

    Routing: Detailseite leer, Routen, Links

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { booksRoutes } from './books/books.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // bei Weiterleitung vom leeren Pfad: (fast) immer pathMatch:full nötig
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  ...booksRoutes,
+  // { path: '**', component: NotFoundPage } // Wildcard-Route
+];
--- book-rating/src/app/app.ts
+++ book-rating/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashboardPage } from "./books/dashboard-page/dashboard-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DashboardPage],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

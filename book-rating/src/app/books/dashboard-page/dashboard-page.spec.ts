b82c9daf22c0069e89b05fb8f812010009cea97d
Ferdinand Malcher <ferdinand@malcher.media>
2025-06-30 11:36:29 +0200

    Test Dashboard: spyOn

--- book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
+++ book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
@@ -33,4 +33,28 @@ describe('DashboardPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call service.rateUp() for component.doRateUp()', () => {
+    // ARRANGE
+    // Service anfordern (das ist eigentlich unser ratingMock)
+    const ratingHelper = TestBed.inject(BookRatingHelper);
+
+    // Testbuch
+    const testBook = { isbn: '3424' } as Book; // Type Assertion – bitte vorsichtig verwenden!
+
+    // Mock überwachen
+    // spyOn(ratingHelper, 'rateUp').and.returnValue(testBook);
+    // spyOn(ratingHelper, 'rateUp').and.callFake(b => b);
+
+    // Methode überwachen, aber die originale Methode wird trotzdem
+    // verwendet, um den Wert zu erzeugen
+    spyOn(ratingHelper, 'rateUp').and.callThrough();
+
+    // ACT
+    component.doRateUp(testBook);
+
+    // ASSERT
+    // prüfen, ob service.rateUp() aufgerufen wurde
+    expect(ratingHelper.rateUp).toHaveBeenCalledOnceWith(testBook);
+  });
 });

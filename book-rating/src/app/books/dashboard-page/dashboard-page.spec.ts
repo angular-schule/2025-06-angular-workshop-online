1f22088cdae328c854ad6b4306bb194ecbd5437a
Ferdinand Malcher <ferdinand@malcher.media>
2025-06-30 10:44:30 +0200

    Test Dashboard: RatingHelper ersetzen

--- book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
+++ book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardPage } from './dashboard-page';
+import { Book } from '../shared/book';
+import { BookRatingHelper } from '../shared/book-rating-helper';
 
 describe('DashboardPage', () => {
   let component: DashboardPage;
   let fixture: ComponentFixture<DashboardPage>;
 
   beforeEach(async () => {
+
+    const ratingMock = {
+      rateUp: (b: Book) => b,
+      rateDown: (b: Book) => b,
+    };
+
     await TestBed.configureTestingModule({
-      imports: [DashboardPage]
+      imports: [DashboardPage],
+      providers: [
+        // BRH ersetzen: Immer wenn jemand BRH anfordert,
+        // wird stattdessen unser ratingMock ausgeliefert
+        { provide: BookRatingHelper, useValue: ratingMock }
+      ]
     })
     .compileComponents();
 

3852936660c7eeac18d20f15fb4c4bb0dc2c7fd9
Ferdinand Malcher <ferdinand@malcher.media>
2025-07-02 15:09:07 +0200

    Ausblick Forms

--- book-rating/src/app/books/book-create-page/book-create-page.ts
+++ book-rating/src/app/books/book-create-page/book-create-page.ts
@@ -1,9 +1,17 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, isFormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { Book } from '../shared/book';
 import { BookStore } from '../shared/book-store';
 import { Router } from '@angular/router';
+
+const pwValidator: ValidatorFn = function (control) {
+  if(!isFormGroup(control)) {
+    return null;
+  }
+
+  return control.get('pw1')?.value === control.get('pw1')?.value ? null : { pwerror: true };
+}
 
 @Component({
   selector: 'app-book-create-page',
@@ -48,7 +56,33 @@ export class BookCreatePage {
         Validators.min(0)
       ]
     }),
+    /*password: new FormGroup({
+      pw1: new FormControl('', { nonNullable: true }),
+      pw2: new FormControl('', { nonNullable: true }),
+    }, { validators: [pwValidator] })*/
+    authors: new FormArray([
+      new FormControl('', { nonNullable: true }),
+      new FormControl('', { nonNullable: true }),
+      new FormControl('', { nonNullable: true }),
+    ])
   });
+
+  addAuthorControl() {
+    this.bookForm.controls.authors.push(
+      new FormControl('', { nonNullable: true })
+    )
+  }
+
+  setManyAuthors() {
+    const array = this.bookForm.controls.authors;
+    const requiredLength = 5;
+    const rest = requiredLength - array.length;
+    for (let i = 0; i < rest; i++) {
+      this.addAuthorControl();
+    }
+
+    array.patchValue(['A', 'B', 'C', 'D', 'E'])
+  }
 
   isInvalid(c: FormControl): boolean {
     return c.touched && c.invalid;

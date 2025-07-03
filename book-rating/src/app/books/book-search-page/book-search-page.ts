30773b47f33ccffef3f9dc23eee1453b2c301141
Ferdinand Malcher <ferdinand@malcher.media>
2025-07-03 12:03:56 +0200

    Suche: Verbesserung leerer Suchbegriff

--- book-rating/src/app/books/book-search-page/book-search-page.ts
+++ book-rating/src/app/books/book-search-page/book-search-page.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { debounceTime, filter, startWith, switchMap } from 'rxjs';
+import { debounceTime, filter, map, of, startWith, switchMap } from 'rxjs';
 import { BookStore } from '../shared/book-store';
 import { toSignal } from '@angular/core/rxjs-interop';
 
@@ -15,8 +15,15 @@ export class BookSearchPage {
   searchControl = new FormControl('', { nonNullable: true });
 
   protected results = toSignal(this.searchControl.valueChanges.pipe(
-    filter(term => term.length >= 3),
+    // filter(term => term.length >= 3),
     debounceTime(250),
-    switchMap(term => this.#bookStore.search(term))
+    switchMap(term => {
+      if (term.length >= 3) {
+        return this.#bookStore.search(term);
+      } else {
+        return of([]);
+      }
+    }),
+    // switchMap(term => this.#bookStore.search(term))
   ), { initialValue: [] });
 }
